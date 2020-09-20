bf847173d4307f631811d91493b73679b60fb2e1
Prajesh Ananthan <prajesh.ananthan@airasiabig.com>
2020-09-20 18:26:24 +0800

    42. Manipulating the state

--- src/App.js
+++ src/App.js
@@ -8,14 +8,24 @@ class App extends Component {
   // Javascript Object
   state = {
     persons: [
-      { name: 'Prajesh', age: 28 },
+      { name: 'David', age: 28 },
       { name: 'Stephen', age: 29 },
       { name: 'Natasha', age: 32 }
-    ]
+    ],
+    otherState: 'some other value'
   }
 
-  switchNameHandler() {
+  // It does not override the old state. 
+  // Instead it will merge the old state with a new one
+  switchNameHandler = () => {
     console.log("Name changed");
+    this.setState({
+      persons: [
+        { name: 'Prajesh', age: 33 },
+        { name: 'Stephen', age: 29 },
+        { name: 'Stephanie', age: 22 }
+      ]
+    });
   }
 
   // To render HTML code in the screen
@@ -24,10 +34,13 @@ class App extends Component {
     return (
       <div className="App">
         <p>This is really working</p>
+
         <button onClick={this.switchNameHandler}>Switch name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+
+        {this.state.persons.map((value, index) => {
+          return <Person name={this.state.persons[index].name} age={this.state.persons[index].age} />
+        })}
+
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
